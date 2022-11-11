012f5f56a82b49e352abbf02d82218f1de4a1841
John Harver <johnharvey@gmail.com>
2022-11-11 18:47:22 -0500

    hhhh

---
+++ src/test/Test1.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Welcome to practice");
+
+    }
+    public void tset() {
+        System.out.println("hhhhhhhhhhhhhhhhhh");
+    }
+
+}
